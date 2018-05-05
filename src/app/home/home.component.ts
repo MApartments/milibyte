f0afd4ac15fc12b693bebab3f49caf1a72853967
MApartments <app_milic@hotmail.com>
2018-05-05 02:11:10 +0200

    4.5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,44 @@ import {Component, OnInit} from '@angular/core';
   ],
 })
 export class HomeComponent implements OnInit {
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  folders = [
+    {name: '128 slušatelja'},
+    {name: 'Neograničen mjesečni promet'},
+    {name: 'Bitrate 128Kbps'},
+    {name: 'AutoDJ'},
+    {name: 'Prikaz informacija o trenutnom izvođaču'},
+    {name: 'Hrvatski data centar'},
+    {name: 'Administracijski web panel'},
+  ];
+  notes = [
+    {name: '32 slušatelja'},
+    {name: 'Neograničen mjesečni promet'},
+    {name: 'Bitrate 128Kbps'},
+    {name: 'AutoDJ'},
+    {name: 'Prikaz informacija o trenutnom izvođaču'},
+    {name: 'Hrvatski data centar'},
+    {name: 'Administracijski web panel'},
+  ];
   constructor() {}
 
   ngOnInit() {
   }
 
 }
-
-
+export interface Element {
+  name: string;
+  position: string;
+  weight: string;
+  symbol: string;
+}
+const ELEMENT_DATA: Element[] = [
+  {position: 'Diskovni prostor', name: '2.000 MB', weight: '25000', symbol: '150000'},
+  {position: 'Broj web stranica', name: 'Neograničeno', weight: 'Neograničeno', symbol: 'Neograničeno'},
+  {position: 'Odabir PHP verzije (ver. 5.2 do 7.1)', name: 'DA', weight: 'DA', symbol: 'DA'},
+  {position: 'cPanel administracija', name: 'DA', weight: 'DA', symbol: 'DA'},
+  {position: 'MySQL baze', name: '2', weight: '20', symbol: 'Neograničeno'},
+  {position: '99,5% uptime garancija', name: 'DA', weight: 'DA', symbol: 'DA'},
+  {position: 'Cijena godišnje', name: '100Kn', weight: '350Kn', symbol: '1000Kn'},
+];
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,7 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import {LayoutModule} from '../layout/layout.module';
-import {MatButtonModule, MatCardModule, MatDialogModule, MatGridListModule, MatIconModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatChipsModule,
+  MatDialogModule, MatDividerModule,
+  MatGridListModule,
+  MatIconModule, MatListModule,
+  MatTableModule
+} from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {CovalentDataTableModule, CovalentVirtualScrollModule} from '@covalent/core';
 
@@ -21,6 +29,10 @@ import {CovalentDataTableModule, CovalentVirtualScrollModule} from '@covalent/co
     MatIconModule,
     CovalentDataTableModule,
     CovalentVirtualScrollModule,
+    MatChipsModule,
+    MatTableModule,
+    MatDividerModule,
+    MatListModule,
   ],
   declarations: [HomeComponent]
 })
