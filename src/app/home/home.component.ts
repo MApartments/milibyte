9301ac30d174dcadfeec887cc77910376cd5f8da
milibyte <app_milic@hotmail.com>
2018-05-29 00:27:49 +0200

    v2.1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,10 +12,8 @@ import {Hosting} from './hosting';
   ],
 })
 export class HomeComponent implements OnInit {
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
   shoutcasts: Shoutcast[] = [];
-  hostings: Hosting[] = []
+  hostings: Hosting[] = [];
   constructor(private hostingService: HostingService) {}
 
   ngOnInit() {
@@ -31,18 +29,3 @@ export class HomeComponent implements OnInit {
   }
 
 }
-export interface Element {
-  name: string;
-  position: string;
-  weight: string;
-  symbol: string;
-}
-const ELEMENT_DATA: Element[] = [
-  {position: 'Diskovni prostor', name: '2.000 MB', weight: '25000', symbol: '150000'},
-  {position: 'Broj web stranica', name: 'Neograničeno', weight: 'Neograničeno', symbol: 'Neograničeno'},
-  {position: 'Odabir PHP verzije (ver. 5.2 do 7.1)', name: 'DA', weight: 'DA', symbol: 'DA'},
-  {position: 'cPanel administracija', name: 'DA', weight: 'DA', symbol: 'DA'},
-  {position: 'MySQL baze', name: '2', weight: '20', symbol: 'Neograničeno'},
-  {position: '99,5% uptime garancija', name: 'DA', weight: 'DA', symbol: 'DA'},
-  {position: 'Cijena godišnje', name: '100Kn', weight: '350Kn', symbol: '1000Kn'},
-];
