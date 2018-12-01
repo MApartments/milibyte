9d20803104e07c9097c7a47e8431c0c7e2a7105d
milibyte <app_milic@hotmail.com>
2018-12-01 14:09:22 +0100

    v2.2

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -21,11 +21,24 @@ export class TestComponent implements OnInit {
 
   dodaj(proizvod: string) {
     console.log(proizvod);
-    this.products.push(proizvod);
+    if (this.checkIsEmptyString(proizvod) === false) {
+      this.products.push(proizvod);
+    }
+    else {
+      alert ('greska')
+    }
   }
   
   remove(proizvod: string) {
     this.products = this.products.filter(product => product !== proizvod);
     console.log(proizvod);
+  }
+
+  checkIsEmptyString(value: string): boolean {
+    if(typeof value !== 'string' || value === '') {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
